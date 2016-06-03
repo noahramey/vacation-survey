28701e75990a56d76051bed43602e09359849757
Noah Ramey <noahramey1997@gmail.com>
2016-06-03 11:01:00 -0700

    finish basic javascript, html, and styles. Will be adding further questions to form, adding more styles, and possibly more locations.

--- js/scripts.js
+++ js/scripts.js
@@ -6,27 +6,33 @@ $(document).ready(function() {
     var insideOutside = parseInt($("#inside-outside").val());
     var budget = parseInt($("#budget").val());
     var siteseeingActivities = parseInt($("#siteseeing-activities").val());
+    var name = $("#name").val();
 
     $("#stregis, #altapura, #lake-tahoe, #himalayas, #maui, #gracebay").hide();
 
     if (mountainsBeach === 1 && snowSun === 1 && insideOutside === 1) {
       $("#stregis").show();
+      $(".name").text(name);
     }
 
     if (mountainsBeach === 1 && snowSun === 1 && insideOutside === 2) {
       $("#altapura").show();
+      $(".name").text(name);
     }
 
     if (mountainsBeach === 1 && snowSun === 2 && insideOutside === 1) {
       $("#lake-tahoe").show();
+      $(".name").text(name);
     }
 
     if (mountainsBeach === 1 && snowSun === 2 && insideOutside === 2) {
       $("#himalayas").show();
+      $(".name").text(name);
     }
 
     if (mountainsBeach === 2 && snowSun === 2 && insideOutside === 1) {
       $("#maui").show();
+      $(".name").text(name);
     }
 
     event.preventDefault();
