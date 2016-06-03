beb7ca9440b83092e86a63c5b2f4fd25bad2f2ef
Noah Ramey <noahramey1997@gmail.com>
2016-06-03 13:56:09 -0700

    fix javascript bug

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
     var budget = parseInt($("#budget").val());
     var siteseeingActivities = parseInt($("#siteseeing-activities").val());
     var name = $("#name").val();
+    var question5Answer = $("#question5").val();
 
     $("#stregis, #altapura, #lake-tahoe, #himalayas, #maui, #gracebay, #snowybeach").hide();
 
@@ -33,12 +34,14 @@ $(document).ready(function() {
     } else if (mountainsBeach === 2 && snowSun === 2 && insideOutside === 2) {
       $("#survey").fadeOut();
       $("#gracebay").fadeIn();
-      (".name").text(name);
-    } else {
+      $(".name").text(name);
+    } else if (mountainsBeach === 2 && snowSun === 1) {
       $("#survey").fadeOut();
       $("#snowybeach").fadeIn();
       $(".name").text(name);
     }
+
+    alert("hi")
 
     event.preventDefault();
   });
